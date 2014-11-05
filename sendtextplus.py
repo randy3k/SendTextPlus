8a618df59f19ef35ffbe83782bfd139c63ee5406
Randy Lai <randy.cs.lai@gmail.com>
2014-11-05 01:28:52 -0800

    don't send empty line and more robust application name match

--- sendtextplus.py
+++ sendtextplus.py
@@ -35,17 +35,19 @@ def escape_dq(cmd):
     return cmd
 
 def sendtext(prog, cmd):
+    if cmd.strip() == "":
+        return
     plat = sublime.platform()
     settings = sublime.load_settings("SendTextPlus.sublime-settings")
 
-    if prog == 'Terminal.app':
+    if re.match('Terminal', prog):
         cmd = clean(cmd)
         cmd = escape_dq(cmd)
         args = ['osascript']
         args.extend(['-e', 'tell app "Terminal" to do script "' + cmd + '" in front window\n'])
         subprocess.Popen(args)
 
-    elif prog == "iTerm.app":
+    elif re.match('iTerm', prog):
         cmd = clean(cmd)
         cmd = escape_dq(cmd)
         # when cmd ends in a space, iterm does not execute. Thus append a line break.
@@ -54,7 +56,6 @@ def sendtext(prog, cmd):
         args = ['osascript']
         args.extend(['-e', 'tell app "iTerm" to tell the first terminal to tell current session to write text "' + cmd +'"'])
         subprocess.Popen(args)
-
 
     elif prog == "tmux":
         progpath = settings.get("tmux", "tmux")
