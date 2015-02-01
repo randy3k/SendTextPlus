39b69630406d4b4d9cdde08549248c4147177391
Randy Lai <randy.cs.lai@gmail.com>
2015-01-31 19:32:38 -0800

    include new line symbols

--- sendtextplus.py
+++ sendtextplus.py
@@ -76,7 +76,7 @@ def sendtext(view, cmd):
         cmd = clean(cmd)
         cmd = escape_dq(cmd)
         cmd = cmd.split("\n")
-        line_len = [len(c) for c in cmd]
+        line_len = [len(c)+1 for c in cmd]
         k = 0
         ver = iTerm_version()
         while k < len(line_len):
