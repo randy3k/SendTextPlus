d631e8e8790ad18b9a49618495a5fcea675f6b2b
Randy Lai <randy.cs.lai@gmail.com>
2015-11-14 15:38:18 -0500

    focus on the next cell

--- sendtext+.py
+++ sendtext+.py
@@ -170,10 +170,10 @@ class TextSender:
                 set URL of front window's active tab to "javascript:{" & "
                     var mycell = IPython.notebook.get_selected_cell();
                     mycell.set_text(\\"" & item 1 of argv & "\\");
-                    IPython.notebook.scroll_to_cell( IPython.notebook.find_cell_index(mycell));
                     mycell.execute();
-                    IPython.notebook.insert_cell_below();
+                    var nextcell = IPython.notebook.insert_cell_below();
                     IPython.notebook.select_next();
+                    IPython.notebook.scroll_to_cell(IPython.notebook.find_cell_index(nextcell));
                 " & "}"
             end tell
         end run
@@ -192,10 +192,10 @@ class TextSender:
                 tell front window's current tab to do JavaScript "
                     var mycell = IPython.notebook.get_selected_cell();
                     mycell.set_text(\\"" & item 1 of argv & "\\");
-                    IPython.notebook.scroll_to_cell( IPython.notebook.find_cell_index(mycell));
                     mycell.execute();
-                    IPython.notebook.insert_cell_below();
+                    var nextcell = IPython.notebook.insert_cell_below();
                     IPython.notebook.select_next();
+                    IPython.notebook.scroll_to_cell(IPython.notebook.find_cell_index(nextcell));
                 "
             end tell
         end run
