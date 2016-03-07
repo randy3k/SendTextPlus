a64bc295165dd6bae748f6e1cc3449799e3dfdc8
Randy Lai <randy.cs.lai@gmail.com>
2016-03-06 23:39:41 -0500

    need to set thread to None

--- textsender.py
+++ textsender.py
@@ -64,14 +64,16 @@ class TextSender:
             cls.cb = sublime.get_clipboard()
         else:
             cls.thread.cancel()
+            cls.thread = None
         sublime.set_clipboard(cmd)
 
     @classmethod
     def reset_clipboard(cls):
         def _reset_clipboard():
-            if cls.cb:
+            if cls.cb is not None:
                 sublime.set_clipboard(cls.cb)
             cls.cb = None
+            cls.thread = None
         cls.thread = threading.Timer(0.5, _reset_clipboard)
         cls.thread.start()
 
